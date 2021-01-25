c969e9d03be90f79beb61d0526d637217c6eef22
padget <padget.pro@gmail.com>
2021-01-25 14:48:54 +0000

    templating library with config type

--- model.hpp
+++ model.hpp
@@ -1,13 +1,30 @@
 #ifndef __clon_model_hpp__
 #define __clon_model_hpp__
 
-#include <string_view>
-#include <vector>
 #include <variant>
 #include <concepts>
 
-namespace clon::model
+namespace clon::model::v2
 {
+  template<
+    typename number_t, 
+    typename string_t, 
+    typename boolean_t,
+    typename object_t, 
+    typename name_t, 
+    typename buffer_t, 
+    typename view_t>
+  struct config
+  {
+    using number_type = number_t;
+    using string_type = string_t;
+    using object_type = object_t;
+    using boolean_type = boolean_t;
+    using name_type = name_t;
+    using buffer_type = buffer_t;
+    using view_type = view_t;
+  };
+
   enum struct clon_type : int
   {
     none = 0,
@@ -17,60 +34,81 @@ namespace clon::model
     object = 4
   };
 
-  struct node;
+  template<typename config_t>
+  using number_type = typename config_t::number_type;
 
-  using none = std::monostate;
-  using number = double;
-  using boolean = bool;
-  using string = std::string_view;
-  using object = std::vector<node>;
+  template<typename config_t>
+  using string_type = typename config_t::string_type;
+  
+  template<typename config_t>
+  using boolean_type = typename config_t::boolean_type; 
+  
+  template<typename config_t>
+  using object_type = typename config_t::object_type;
 
+  template<typename config_t>
+  using none_type  = std::monostate;
+
+  template<typename config_t>
+  using name_type = typename config_t::name_type;
+
+  template<typename config_t>
+  using buffer_type = typename config_t::buffer_type;
+
+  template<typename config_t>
+  using view_type = typename config_t::view_type;
+
+  template<typename config_t>
   using clon_value = std::variant<
-    std::monostate, boolean,
-    number, string, object>;
+    none_type<config_t>, 
+    boolean_type<config_t>, 
+    number_type<config_t>, 
+    string_type<config_t>, 
+    object_type<config_t>>; 
 
+  template<typename config_t>
   struct node
   {
-    std::string_view name;
-    clon_value val;
+    name_type<config_t> name;
+    clon_value<config_t> val;
   };
 
+  template<typename config_t>
   struct root
   {
-    std::vector<char> buff;
-    node root;
+    buffer_type<config_t> buff;
+    node<config_t> root;
   };
 }
 
-namespace clon::constraint
+namespace clon::model::v2::constraint
 {
-  template<typename type_t>
-  concept number =
-    std::integral<type_t> or
-    std::floating_point<type_t>;
-
-  template<typename type_t>
-  concept char_sequence =
-    std::same_as<type_t, std::vector<char>> or
-    std::same_as<type_t, std::string> or
-    std::same_as<type_t, std::string_view>;
-
-  template<typename type_t>
+  template<typename config_t, typename type_t>
   concept possible_value =
-    std::same_as<type_t, clon::model::none> or
-    std::same_as<type_t, clon::model::object> or
-    std::same_as<type_t, clon::model::string> or
-    std::same_as<type_t, clon::model::number> or
-    std::same_as<type_t, clon::model::boolean>;
+    std::same_as<type_t, clon::model::v2::none_type<config_t>> or
+    std::same_as<type_t, clon::model::v2::boolean_type<config_t>> or
+    std::same_as<type_t, clon::model::v2::number_type<config_t>> or
+    std::same_as<type_t, clon::model::v2::string_type<config_t>> or
+    std::same_as<type_t, clon::model::v2::object_type<config_t>>;
 }
 
-namespace clon::basic
+namespace clon::basic::v2
 {
-  clon::model::node& undefined();
-  clon::model::clon_type type(const clon::model::node& c);
+  template<typename config_t>
+  clon::model::v2::node<config_t>& undefined()
+  {
+    static clon::model::v2::node<config_t>{} u;
+    return u; 
+  }
 
-  template<clon::constraint::possible_value type_t>
-  const bool is_(const clon::model::node& c)
+  template<typename config_t>
+  clon::model::v2::clon_type type(const clon::model::v2::node<config_t>& c)
+  {
+    return (clon::model::v2::clon_type) c.val.index();
+  }
+
+  template<typename config_t, clon::model::v2::constraint::possible_value<config_t> type_t>
+  const bool is_(const clon::model::v2::node<config_t>& c)
   {
     if constexpr (std::is_same_v<type_t, clon::model::boolean>)
       return type(c) == clon::model::clon_type::boolean;
@@ -84,11 +122,14 @@ namespace clon::basic
       return type(c) == clon::model::clon_type::none;
   }
 
-  template<clon::constraint::possible_value type_t>
-  const type_t& as_(const clon::model::node& c)
+  template<typename config_t, clon::model::v2::constraint::possible_value<config_t> type_t>
+  const type_t& as_(const clon::model::v2::node<config_t>& c)
   {
     return std::get<type_t>(c.val);
   }
 }
+
+
+
 
 #endif 
--- parsing.hpp
+++ parsing.hpp
@@ -2,7 +2,6 @@
 #define __clon_parsing_hpp__
 
 #include "model.hpp"
-#include <exception>
 #include <string_view>
 #include <stdexcept>
 #include <fmt/format.h>
@@ -21,6 +20,24 @@ namespace clon::parsing
   {
     return parse(fmt::format(pattern, as...));
   }
+}
+
+namespace clon::parsing::v2
+{
+  template<typename config_t>
+  class expected_character : public std::invalid_argument
+  {
+  public:
+    expected_character(const clon::model::v2::view_type<config_t>& chars):
+      
+  };
+
+  template<typename config_t>
+  clon::model::v2::root<config_t> parse(const clon::model::v2::view_type<config_t>& v)
+  {
+
+  }
+
 }
 
 #endif 
